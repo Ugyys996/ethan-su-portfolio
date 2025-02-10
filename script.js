55cabe93bea3e2e211e4a820f0a01c9dd3214eab
Ugyys996 <155803288+Ugyys996@users.noreply.github.com>
2025-02-10 09:54:17 +0800

    更新 script.js

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-document.addEventListener("DOMContentLoaded", function() {
-    console.log("网站已加载");
-});
+// 获取导航链接和各个部分
+const navLinks = document.querySelectorAll('nav ul li a');
+const sections = document.querySelectorAll('.section');
+
+// 当点击导航项时，显示相应的内容
+navLinks.forEach(link => {
+    link.addEventListener('click', (event) => {
+        event.preventDefault();
+        
+        // 隐藏所有内容区域
+        sections.forEach(section => {
+            section.classList.remove('active');
+        });
+        
+        // 获取目标部分的ID
